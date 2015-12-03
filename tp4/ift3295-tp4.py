6df07e6d8985a047af1b3798d7bcf8ce40ae3c58
DjAntaki <antaki.vincent@gmail.com>
2015-12-03 16:55:29 -0500

    Structure et itérateur done. Faut faire le loading de mutation.txt

--- tp4/ift3295-tp4.py
+++ tp4/ift3295-tp4.py
@@ -5,17 +5,18 @@ class node:
     next_unique_id = 0
 
     def __init__(self,content=None,parent=None,childrens=[]):
-        
-        if parent is not None:
-            parent.childrens.append(self)
         self.parent = parent
-        
         self.id = node.next_unique_id
         node.next_unique_id += 1
         self.content = content
         self.childrens = childrens
-
-
+        
+    def __str__(self):
+        if self.content is None:
+            return str(self.id) 
+        else :
+            return str(self.id) + " " + str(self.content)
+            
 class binary_tree:
     def __init__(self, newick_expr):
         self.leafs = []
@@ -24,25 +25,23 @@ class binary_tree:
 
     def _parse_newick(self, expr, parent=None):
         """parse a newick tree to this representation"""
-        print('i',expr)
         if expr[0] == '(':
             assert expr[-1] == ')',"Erreur de parentheses"
             e = node(parent=parent)
-    	    parCount=0
-    	    for i in range(len(expr)):
-		        if(parCount==1 and expr[i]==','):
-		            virgPos=i
-		            break
-		        if(expr[i]=='('):
-		            parCount+=1
-		        elif(expr[i]==')'):
-		            parCount-=1
+            parCount=0
+            for i in range(len(expr)):
+                if(parCount==1 and expr[i]==','):
+                    virgPos=i
+                    break
+                if(expr[i]=='('):
+                    parCount+=1
+                elif(expr[i]==')'):
+                    parCount-=1
             a = expr[1:virgPos] #Trouver la virgule du milieu et se debarasser des parentheses.
-	        b = expr[virgPos+1:-1]
-	        print("a et b:")
-	        print(a)
-	        print(b)
+            b = expr[virgPos+1:-1]
+            print('b',b)
             a,b = self._parse_newick(a,e), self._parse_newick(b,e)        
+            e.childrens = [a,b]
             return e
         else :
             e = node(expr,parent)
@@ -51,21 +50,47 @@ class binary_tree:
         
     def reset(self):
         """ Lorsque appelé, cette fonction retourne la fringe à son état initial et ne considère aucun noeud comme étant visité. """
-        self.search_fringe = self.leafs.copy()
+        self.search_fringe = list(self.leafs)
         self.checked = set() 
+        self.last = None
     
+    def __iter__(self):
+        """ """
+        return self
+
     def __next__(self):
-    """ renvoit le prochain noeud, """
-        if self.last is not None :
+        return self.next
+    
+    def next(self):
+        """ renvoit le prochain noeud, """
+        if self.last is not None and self.last.parent is not None:
+        
             if all(i.id in self.checked for i in self.last.parent.childrens):
                 self.search_fringe.append(self.last.parent)
                 
+        
         if len(self.search_fringe) == 0 :
             raise StopIteration
         else :
             self.last = self.search_fringe.pop(0)
             self.checked.add(self.last.id)
-            yield self.last        
+            return self.last        
+
+#def getSequence(string,p) :
+#    """Kescéça?"""
+#	p.seek(0)	
+#	seq=''
+#	take=False
+#	for line in p:
+#		print(line)
+#		if line[0]=='>':
+#			if line[1:-1]==string:
+#				take=True
+#			else:
+#				take=False
+#		elif(take):
+#			seq+=line[:-1]
+#	return seq
 
 newick_trees = []
 
@@ -76,11 +101,14 @@ if __name__ == '__main__':
     print(sys.argv)
     f = open(sys.argv[1], 'rb')
     for line in f:
-        if all(i in line for i in '(',',',')'):  
-            line = line[:-2] #Stripping the line of ; and /n
-            print('aa', line)
+        if all([i in line for i in set(['(',',',')'])]):  
+            line = line[:-3] #Stripping the line of ; and /n
+            line = line.replace(' ','')
             newick_trees.append(binary_tree(line))            
+        break
             
 print(newick_trees)
 #    w = sys.argv[2]
+for i in newick_trees[0]:
+    print(i)
 
