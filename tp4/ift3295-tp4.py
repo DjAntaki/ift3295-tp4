0e38e872000d1a79d029e80bbb7f5b0d5af36fe2
DjAntaki <antaki.vincent@gmail.com>
2015-12-05 17:09:43 -0500

    version qui marche

--- tp4/ift3295-tp4.py
+++ tp4/ift3295-tp4.py
@@ -13,6 +13,7 @@ class node:
         self.childrens = childrens
         self.sequence=[]
 
+
            
     def __str__(self):
         if self.content is None:
@@ -20,26 +21,36 @@ class node:
         else :
             return str(self.id) + " " + str(self.content)
             
-    def sankoff(self):
-        if self.childrens=[]:
-            pass
+    def sankoff(self,index):
+        if len(self.childrens)==0:
+            #retourne un vecteur ou tous les elements sont np. (infini) sauf l'index correspondant au caractère à l'index i
+          
+            nullindex = characters.index(self.sequence[index])
+            w = np.array([np.inf if i != nullindex else 0 for i in range(len(characters))])
+          
+            return w
         else:
             sankoffList=[]
             for c in self.childrens:
-                sankoffList.append(c.sankoff())
+                sankoffList.append(c.sankoff(index))
             assert len(sankoffList)==2
             sank=np.zeros(len(characters))
             for i in range(len(sank)):                
                 #min_left=-1
                 #definir fonction qui retourne le vecteur de transitions
                 min_left=np.min(sankoffList[0]+mutations[i])            
-            
+                min_right=np.min(sankoffList[1]+mutations[i])
+                sank[i] = min_left+min_right
+            return sank
+                        
 
 class binary_tree:
     def __init__(self, newick_expr):
-        self.leafs = []
+        self.leaves = []
         self.root = self._parse_newick(newick_expr)        
         self.reset()
+        for i in self.leaves:
+            i.sequence = getSequence(i.content)        
 
     def _parse_newick(self, expr, parent=None):
         """parse a newick tree to this representation"""
@@ -57,18 +68,18 @@ class binary_tree:
                     parCount-=1
             a = expr[1:virgPos] #Trouver la virgule du milieu et se debarasser des parentheses.
             b = expr[virgPos+1:-1]
-            print('b',b)
+            
             a,b = self._parse_newick(a,e), self._parse_newick(b,e)        
             e.childrens = [a,b]
             return e
         else :
             e = node(expr,parent)
-            self.leafs.append(e)
+            self.leaves.append(e)
             return e 
         
     def reset(self):
         """ Lorsque appelé, cette fonction retourne la fringe à son état initial et ne considère aucun noeud comme étant visité. """
-        self.search_fringe = list(self.leafs)
+        self.search_fringe = list(self.leaves)
         self.checked = set() 
         self.last = None
     
@@ -97,7 +108,7 @@ class binary_tree:
     def pullSequences(self):
         i=0
         sequences=[]
-        for leaf in self.leafs:
+        for leaf in self.leaves:
             strSeq=leaf.content
             sequences.append(getSequence(strSeq,p))
         return sequences
@@ -119,77 +130,78 @@ class binary_tree:
 #	return seq
 
 
-def getSequence(string,p):
-	p.seek(0)	
-	seq=''
-	take=False
-	for line in p:
-		print(line)
-		if line[0]=='>':
-			if line[1:-1]==string:
-				take=True
-			else:
-				take=False
-		elif(take):
-			seq+=line[:-1]
-	return seq
-
 
 def getMutations():
 	m = open('mutations.txt','rb')
 	print(f[0])	
 
 newick_trees = []
-characters = None
-mutations = None
+
+#if __name__ == '__main__':
+
+import sys
+print("arguments :")
+print(sys.argv)
+
+#Loading proteine values
+p = open(sys.argv[1],'rb')
+
+def getSequence(string):
+    p.seek(0)	
+    seq=''
+    take=False
+    for line in p:
+	    if line[0]=='>':
+		    if line[1:-1]==string:
+			    take=True
+		    else:
+			    take=False
+	    elif(take):
+		    seq+=line[:-1]
+    return seq
+    
+
+#Loading and parsing newick trees
+f = open(sys.argv[2], 'rb')
+for line in f:
+
+    if all([i in line for i in '(',',',')']):  
+        line = line[:-3] #Stripping the line of ; and /n
+        line = line.replace(' ','')
+        newick_trees.append(binary_tree(line))            
+
+#Loading mutations values
 mutation = dict() # (from, to) -> cost
+mut = open(sys.argv[3],'rb').read()
+lines = mut.split('\n')
+characters = lines[0].replace(' ','')
+mutations = np.zeros((len(characters),len(characters)))    
 
-
-if __name__ == '__main__':
-
-    import sys
-    print("arguments :")
-    print(sys.argv)
-    
-    #Loading and parsing newick trees
-    f = open(sys.argv[1], 'rb')
-    for line in f:
-
-        if all([i in line for i in '(',',',')']):  
-            line = line[:-3] #Stripping the line of ; and /n
-            line = line.replace(' ','')
-            newick_trees.append(binary_tree(line))            
-
-    #Loading mutations values
-    mutation = dict() # (from, to) -> cost
-    mut = open(sys.argv[2],'rb').read()
-    lines = mut.split('\n')
-    characters = lines[0].replace(' ','')
-    print(characters)
-    mutations = np.zeros((len(characters),len(characters)))    
-    for y, i in enumerate(lines[1:]):
-        #Thats kinda sketch but is there to remove multiple space
-        last_len = len(i)+1
+for y, i in enumerate(lines[1:]):
+    #Thats kinda sketch but is there to remove multiple space
+    last_len = len(i)+1
+    actual_len = len(i)
+    while last_len != actual_len :
+        i = i.replace('  ', ' ')
+        last_len = actual_len
         actual_len = len(i)
-        while last_len != actual_len :
-            i = i.replace('  ', ' ')
-            last_len = actual_len
-            actual_len = len(i)
-        #End of sketchy part             
-        l = i.split(' ')
-        mutations[y] = l[1:]
-        print(l)
-#        from_ = l[0]
-#        assert(len(l)-1 == len(x_axis))
-#        for index, to in enumerate(x_axis):
-#            mutation[(from_,to)] = int(l[index+1])
-    
-    print(mutation)        
+    #End of sketchy part             
+    l = i.split(' ')
+    mutations[y] = l[1:]
 
-#Début du traitement des données
-#    for l in newick_trees[0].leafs:
-#	l.sequence=getSequence(
-    print('-------ooo---------')
-    for x in newick_trees[0]:
-	print(x)      
-    
+#Preprocess to know the index of sequences that do not have a hole
+sequences = [i.sequence for i in newick_trees[0].leaves]
+good_index = []
+for x in range(np.min([len(i) for i in sequences])):
+    if all([s[x] != '-' for s in sequences]):
+        good_index.append(x) 
+print('goodindexes',good_index)    
+
+
+#Calculating <<<<<<<< for all trees
+for i,tree in enumerate(newick_trees):
+    total_cost = 0
+    for x in good_index:
+        total_cost += np.min(tree.root.sankoff(x))
+    print("Arbre "+str(i)+" : "+str(total_cost))
+
